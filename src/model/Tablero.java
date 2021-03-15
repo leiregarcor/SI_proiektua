d35d71e16b684bdebf3c843c15378e1dc6e39ee0
leire <leiregarciaupv@gmail.com>
2021-03-15 17:56:51 +0100

    enun

--- src/model/Tablero.java
+++ src/model/Tablero.java
@@ -2,16 +2,18 @@ package model;
 
 public class Tablero {
  private Kasila[][] tablero;
- private static Tablero nTablero = null;
+ private static Tablero nTablero = new Tablero(); /** = null */
 
     private Tablero() {
         this.tablero = tablero;
     }
     public static Tablero getNireTablero(){
-        if(Tablero.nTablero==null) {
+       /** if(Tablero.nTablero==null) {
             Tablero.nTablero=new Tablero();
         }
-        return Tablero.nTablero;
+        return Tablero.nTablero; */
+       return nTablero;
+
     }
 
 }
