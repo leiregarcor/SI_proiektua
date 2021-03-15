cc3c2bf496684804a7b1ba9af8d7d10073280a73
Kerman-Sanjuan <kermansanjuanmalax@gmail.com>
2021-03-15 17:41:37 +0100

    Kasilla done

--- src/model/Kasila.java
+++ src/model/Kasila.java
@@ -1,4 +1,76 @@
 package model;
 
 public class Kasila {
+    private int x;
+    private int y;
+    private int koadrante;
+    private int actual;
+    private int predicted;
+    private String aukerak;
+
+    public Kasila(int x, int y, int koadrante, int actual, int predicted, String aukerak) {
+        this.x = x;
+        this.y = y;
+        this.koadrante = koadrante;
+        this.actual = actual;
+        this.predicted = predicted;
+        this.aukerak = aukerak;
+    }
+
+    public int getActual() {
+        return actual;
+    }
+
+    public void setActual(int actual) {
+        this.actual = actual;
+    }
+
+    public int getPredicted() {
+        return predicted;
+    }
+
+    public void setPredicted(int predicted) {
+        this.predicted = predicted;
+    }
+
+
+
+
+
+
+
+    public int getX() {
+        return x;
+    }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
+
+    public int getKoadrante() {
+        return koadrante;
+    }
+
+    public void setKoadrante(int koadrante) {
+        this.koadrante = koadrante;
+    }
+
+    public String getAukerak() {
+        return aukerak;
+    }
+
+    public void setAukerak(String aukerak) {
+        this.aukerak = aukerak;
+    }
+
+
+
 }
