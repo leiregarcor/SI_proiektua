fff867c2c867d877330c92653acd325af3ebc3bb
leire <leiregarciaupv@gmail.com>
2021-05-17 16:47:41 +0200

    fdhd

--- src/view/RankingBista.java
+++ src/view/RankingBista.java
@@ -6,6 +6,10 @@ import java.awt.EventQueue;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
+import javax.swing.JTextPane;
+import javax.swing.JButton;
+import java.awt.FlowLayout;
+import java.awt.GridLayout;
 
 public class RankingBista extends JFrame {
 
@@ -37,6 +41,34 @@ public class RankingBista extends JFrame {
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		contentPane.setLayout(new BorderLayout(0, 0));
 		setContentPane(contentPane);
+		
+		JTextPane textPane = new JTextPane();
+		contentPane.add(textPane, BorderLayout.CENTER);
+		
+		JPanel goikoPanela = new JPanel();
+		contentPane.add(goikoPanela, BorderLayout.NORTH);
+		goikoPanela.setLayout(new GridLayout(0, 3, 0, 0));
+		
+		JButton lehen10 = new JButton("Lehen 10");
+		goikoPanela.add(lehen10);
+		
+		JButton mailaka = new JButton("Mailaka");
+		goikoPanela.add(mailaka);
+		
+		JButton mailaMax = new JButton("Maila max");
+		goikoPanela.add(mailaMax);
+		
+		JPanel behekoPanela = new JPanel();
+		contentPane.add(behekoPanela, BorderLayout.SOUTH);
+		behekoPanela.setLayout(new GridLayout(1, 3, 0, 0));
+		
+		JPanel panel = new JPanel();
+		behekoPanela.add(panel);
+		
+		JPanel panel_1 = new JPanel();
+		behekoPanela.add(panel_1);
+		
+		JButton exitBotoia = new JButton("Exit");
+		behekoPanela.add(exitBotoia);
 	}
-
 }
